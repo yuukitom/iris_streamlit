e3419661910ff096f6d9b535f26e39ae295b57ec
yuki <65431748+yuukitom@users.noreply.github.com>
2022-12-20 22:46:27 +0900

    Library Deletion

--- iris.py
+++ iris.py
@@ -1,7 +1,5 @@
 # 基本ライブラリ
-from pyparsing import col
 import streamlit as st
-import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
